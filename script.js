8704c973e2363da03b13b72766b95e593cfb9ec3
tamaki uno <t.uno@keio.jp>
2023-10-20 19:45:36 +0900

    add html and css and js files

---
+++ script.js
@@ -0,0 +1,4 @@
+'use strict';
+// get html body
+// querySelector() returns the first element that matches a specified CSS selector(s) in the document.
+const body = document.querySelector('body');  
